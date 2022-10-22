c7de31773a9c25d7448a10adbb50e55789f12a57
maindishes <junyopar@student.42seoul.kr>
2022-10-22 08:27:19 +0000

    Add : images, container delete all

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo docker stop $(sudo docker ps -qa); 
+sudo docker rm $(sudo docker ps -qa);
+sudo docker rmi -f $(sudo docker images -qa); 
+sudo docker volume rm $(sudo docker volume ls -q);
