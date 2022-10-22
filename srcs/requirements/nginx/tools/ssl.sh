865238b05fbaafbf25dcc905766a6cf41a5520af
maindishes <junyopar@student.42seoul.kr>
2022-10-22 08:28:05 +0000

    Add: nginx single container

--- srcs/requirements/nginx/tools/ssl.sh
+++ srcs/requirements/nginx/tools/ssl.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
-    mkdir /etc/nginx/ssl
+    sed -i s/'{$DOMAIN_NAME}'/junyopar@42.fr/g /etc/nginx/sites-available/default
+    mkdir -p /etc/nginx/ssl/private
+    mkdir -p /etc/nginx/ssl/certs
 
 	openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
-    -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Gun/CN=localhost" \
-    -out /etc/nginx/ssl/private/nginx.crt \
-    -keyout /etc/nginx/ssl/certs/nginx.key
+    -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Gun/CN=junyopar.42.fr" \
+    -out /etc/nginx/ssl/certs/nginx.crt \
+    -keyout /etc/nginx/ssl/private/nginx.key
 
 
-exec $@
+exec "$@"
