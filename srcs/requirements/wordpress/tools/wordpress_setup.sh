1640db14123b27b13b8eeee295185ba426a6f9f0
maindishes <junyopar@student.42seoul.kr>
2022-11-01 11:31:22 +0000

    Add: implement wordpress ...

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -11,6 +11,7 @@ chown -R www-data:www-data /var/www;
 # 이방법 말고 wordpress 파일 권한을 www-data 한테 주는걸로~ chown 으로 해보자 
 
 #  얘가 깔려있는지 없는지 확인 해서 없을때만 깔기? 
-sudo -u www-data wp core download --path=/var/www/html/wordpress;
-sudo -u www-data wp config create --dbname=$WP_DB_HOST --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST 
+sudo -u www-data wp core download --path=/var/www/html
+
+sudo -u www-data wp config create --dbname=$WP_DATABASE --dbuser=$WP_ADMIN= --dbpass=$WP_ADMIN_PWD --dbhost=mariadb
 sudo -u www-data wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --path=/var/www/html/wordpress
