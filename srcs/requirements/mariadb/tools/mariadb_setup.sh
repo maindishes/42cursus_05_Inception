f6bf1844466e6902d4b7b433f91d1b5a88bb8309
maindishes <junyopar@student.42seoul.kr>
2022-11-02 11:29:37 +0000

    Add: mariadb root password setting

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -15,21 +15,18 @@ sleep 2;
 # 이건 mariadb의 유저를 만드는것이다. 또는 외부에서 접속 가능하게 하는 User만드는것 
 # wordpress 설정할때 mydb의 유저를 만드는것이고! 
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DATABASE";
-# mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$WP_ADMIN_PWD'";
-# mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '$WP_ADMIN_PWD'";
-# mysql&gt; UPDATE user SET password=PASSWORD('123') WHERE user='root';
-#  mysqladmin -u root password '변경암호'
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$WP_ADMIN'@'%' IDENTIFIED BY '$WP_ADMIN_PWD'";
-mysql -e "GRANT ALL PRIVILEGES ON $WP_DATABASE.* TO '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PWD'";
-mysql -e "UPDATE mysql.user SET plugin = 'mysql_native_'";
-mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$WP_ADMIN_PWD')";
-mysql -e "FLUSH PRIVILEGES";
+sed -i s/'password ='/"password = $WP_ADMIN_PWD"/g /etc/mysql/debian.cnf
+
+mysql -uroot -p$WP_ADMIN_PWD -e "CREATE DATABASE IF NOT EXISTS $WP_DATABASE";
+mysql -uroot -p$WP_ADMIN_PWD -e "GRANT ALL PRIVILEGES ON *.* TO '$WP_ADMIN'@'%' IDENTIFIED BY '$WP_ADMIN_PWD'";
+mysql -uroot -p$WP_ADMIN_PWD -e "GRANT ALL PRIVILEGES ON $WP_DATABASE.* TO '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PWD'";
+mysql -uroot -p$WP_ADMIN_PWD -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$WP_ADMIN_PWD')";
+mysql -uroot -p$WP_ADMIN_PWD -e "FLUSH PRIVILEGES";
 
 # mysql < /tmp/mysql.sql;
 # eval "echo \"$(cat /tmp/mysql.sql)\"" | mysql
-# service mysql stop;
-/usr/bin/mysqladmin -u root -p123 shutdown
+service mysql stop;
+# /usr/bin/mysqladmin -u root -p123 shutdown;
 
 exec "$@";
 
